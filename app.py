f472bf41a90014e616beebbc54eaf19666259936
Armstrongthedolphin <1056608361@qq.com>
2018-03-20 21:44:58 -0400

    Added react
    CORS nor solved

--- app.py
+++ app.py
@@ -1,18 +1,57 @@
 import flask
-from flask import Flask, request, url_for, jsonify, json
+from flask import Flask, request, url_for, jsonify, json, make_response,current_app
 from flask_restful import reqparse, abort, Api, Resource
+from flask_cors import CORS, cross_origin
+from datetime import timedelta
+from functools import update_wrapper
+from bottle import route, app
 
 import Exporter
+#
+# def after_request(response):
+#     response.headers['Access-Control-Allow-Origin'] = '*'
+#     response.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
+#     response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+#     return response
 
 app=Flask(__name__)
+# app.after_request(after_request)
+#CORS(app)
+# app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
+# app.config['CORS_HEADERS'] = 'Content-Type'
+#
+CORS(app, resources={r"/foo": {"origins": "http://localhost:3000"}})
 
 
 @app.route('/',methods=['GET','POST'])
 def index():
 	return flask.send_from_directory('static', 'index.html')
 
+@app.route('/api/test', methods=['GET'])
+# @cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
+# @cross_origin(origin='localhost')
+def template():
+	# usernameArg=request.get_json()
+	# user=usernameArg['user']
+	# print(username)
+	# # # response=jsnofy(user="joe")
+	# # bottle.response.set_header("Access-Control-Allow-Origin", "*")
+    # # bottle.response.set_header("Allow-Methods", "GET, POST, OPTIONS")
+    # # bottle.response.set_header("Access-Control-Allow-Headers", "Origin, Content-Type")
+    # 	return jsonify(user="joe")
+	if request.method == 'OPTIONS':
+	    res = flask.make_response()
+	if request.method == 'POST':
+	    data = request.data
+	    data = json.loads(data)
+	    res = flask.make_response(data)
+	# res.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+	res.headers['Access-Control-Allow-Methods'] = 'POST,GET,OPTIONS'
+	res.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
+	return res
 
-@app.route('/byUserName',methods=['GET','POST'])
+@app.route('/api/byUserName',methods=['GET'])
+# @cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def scrapeByUser():
 	if request.headers['Content-Type']=='application/json':
 		usernameArg=request.get_json()
@@ -33,7 +72,7 @@ def getSentiment():
 @app.route('/getCSV', methods=['POST'])
 def getCSV():
 
-	# request.data = file_name="file" 
+	# request.data = file_name="file"
 
 	if (request.headers['Content-Type']=='application/json'):
 
---
+++ static/src/App.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import DatePicker from 'material-ui/DatePicker';
+//import axios from 'axios';
+import $ from 'jquery'
+
+// var user='';
+// var tweet_amount='';
+// var file_name='';
+// var keyword='';
+//var origin = window.location.origin;
+class App extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     user: '',
+  //     tweet_amount:'',
+  //     file_name:'',
+  //     keyword:'',
+  //   };
+  // }
+  render() {
+    //const{user,tweet_amount,file_name,keyword} = this.state;
+    return (
+    <MuiThemeProvider>
+      <div className="App">
+        <AppBar
+          title="IDEAHub"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <TextField hintText="Username" floatingLabelText="Username"
+                type="text" id="user"
+                onChange={this.handleChange.bind(this)}/><br/>
+          <TextField hintText="Tweets amount" floatingLabelText="Tweets amount"
+                type="text" id="tweet_amount"
+                onChange={this.handleChange.bind(this)}/><br/>
+          <TextField hintText="CSV file name" floatingLabelText="CSV file name"
+                type="text" id="filename"
+                onChange={this.handleChange.bind(this)}/><br/>
+          <TextField hintText="Keyword" floatingLabelText="Keyword"
+                type="text" id="keyword"
+                onChange={this.handleChange.bind(this)}/><br/>
+          <DatePicker hintText="Start Date" id="start_date"/>
+          <DatePicker hintText="End Date" id="end_date"/>
+          <RaisedButton label="Submit" type="submit"/>
+        </form>
+      </div>
+    </MuiThemeProvider>
+    );
+  }
+  handleChange(e,index,value){
+    var name=e.target.id
+    this.setState({[e.target.id]: index});
+    //console.log("test",this.state.name);
+  }
+  handleSubmit(e){
+    e.preventDefault();
+    //var url =  origin + '/byUserName'
+    // $.ajax({
+    //   url: 'http://127.0.0.1:5000/api/test',
+    //   dataType: 'json',
+    //   type: 'POST',
+    //   data: JSON.stringify(this.state),
+    //   contentType: 'application/json; charset=utf-8',
+    //   success: function(data){
+    //     alert(data.message);
+    //   }.bind(this),
+    //   error: function(xhr, status, err){
+    //     alert('Poll creation failed: ' + err.toString());
+    //   }.bind(this)
+    // });
+    //const { user,tweet_amount,file_name,keyword } = this.state;
+    //alert(this.state);
+    console.log(this.state.user);
+    fetch('http://127.0.0.1:5000/api/test', {
+      method: 'POST',
+      headers: new Headers(
+         {"Content-Type": "application/json"}
+      ),
+      body: JSON.stringify({user:'myname'}),
+    }).then(function (result) {
+    console.log(result);
+    });
+  }
+}
+
+export default App;
