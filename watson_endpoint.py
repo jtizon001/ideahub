9310fbae3593d0e9b57c8f91a228f8ad9689bd86
jtizon001 <tizon.jonathan@gmail.com>
2018-05-06 20:41:09 -0400

    Database and sessions

--- watson_endpoint.py
+++ watson_endpoint.py
@@ -1,9 +1,5 @@
 import json
-<<<<<<< Updated upstream
 from watson_developer_cloud import NaturalLanguageUnderstandingV1,WatsonException
-=======
-from watson_developer_cloud import NaturalLanguageUnderstandingV1
->>>>>>> Stashed changes
 from watson_developer_cloud.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions, SentimentOptions, EmotionOptions
 from csv_manipulator import TweetCsv
 import codecs
@@ -57,30 +53,3 @@ def store_file(name, data):
     print('Sent analysis complete: "%s"' % json.dumps(data, sort_keys=True, indent=4))
     output.close()
     print('Sent analysis stored...')
-<<<<<<< Updated upstream
-=======
-
-#from watson_developer_cloud.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
-
-#natural_language_understanding = NaturalLanguageUnderstandingV1(
-#  username="cacede17-f1de-4533-bde9-7f3554bf7030",
-#  password="frAC0V0rUWTD",
-#  version='2017-02-27')
-
-#response = natural_language_understanding.analyze(
-#  text='IBM is an American multinational technology company '
-#       'headquartered in Armonk, New York, United States, '
-#       'with operations in over 170 countries.',
-#  features=Features(
-#    entities=EntitiesOptions(
-#      emotion=True,
-#      sentiment=True,
-#      limit=2),
-#    keywords=KeywordsOptions(
-#      emotion=True,
-#      sentiment=True,
-#      limit=2)))
-
-#print(json.dumps(response, indent=2))
-
->>>>>>> Stashed changes
